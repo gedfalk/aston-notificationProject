ef6d7cf12987575f7d977347308ab680f876c520
gedfalk <blite.co.stn@gmail.com>
2026-01-28 02:45:30 +0300

    docs: задокументированы EmailController и EmailRequestDto

--- src/main/java/dev/gedfalk/notificationservice/controller/EmailController.java
+++ src/main/java/dev/gedfalk/notificationservice/controller/EmailController.java
@@ -2,6 +2,9 @@ package dev.gedfalk.notificationservice.controller;
 
 import dev.gedfalk.notificationservice.dto.EmailRequestDto;
 import dev.gedfalk.notificationservice.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +22,15 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping("/send")
+    @Operation(
+            summary = "Отправка письма",
+            description = "Отправляет email-сообщение по указанному адресу. Требуется корректный адрес и текст сообщения"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Email успешно отправлен"),
+            @ApiResponse(responseCode = "400", description = "Не удалось отправить email. Некорректныые данные"),
+            @ApiResponse(responseCode = "500", description = "Ошибка сервера")
+    })
     public ResponseEntity<String> sendEmail(@RequestBody EmailRequestDto request) {
         try {
             emailService.sendCustomEmail(request.getEmailTo(), request.getTitle(), request.getMessage());
--- src/main/java/dev/gedfalk/notificationservice/dto/EmailRequestDto.java
+++ src/main/java/dev/gedfalk/notificationservice/dto/EmailRequestDto.java
@@ -1,5 +1,6 @@
 package dev.gedfalk.notificationservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -15,11 +16,14 @@ public class EmailRequestDto {
 
     @NotBlank(message = "Требуется ввести почтовый ящик")
     @Email(message = "Некорректный формат почтового ящика")
+    @Schema(description = "Почтовый ящик пользователя", example = "joerogan@gmail.com")
     private String emailTo;
 
     @NotBlank(message = "Требуется ввести тему письма")
+    @Schema(description = "Тема письма", example = "Вам Бандероль!")
     private String title;
 
     @NotBlank(message = "Требуется ввести тело письма")
+    @Schema(description = "Содержание письма", example = "Это содержание письма. Дело было так...")
     private String message;
 }
