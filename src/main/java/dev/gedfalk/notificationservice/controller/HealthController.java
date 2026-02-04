2791cb9fd61f18bc84c15e07f5ddcd5585ed69ad
gedfalk <blite.co.stn@gmail.com>
2026-02-04 06:41:21 +0300

    feat: добавлен Health check контроллер

---
+++ src/main/java/dev/gedfalk/notificationservice/controller/HealthController.java
@@ -0,0 +1,22 @@
+package dev.gedfalk.notificationservice.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Health Check", description = "Проверка базовой работоспособности user-notification API")
+public class HealthController {
+
+    @GetMapping("/health")
+    @Operation(summary = "Проверка 'здоровья' приложения")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Приложение работает корреткно"
+    )
+    public String health() {
+        return "Всё чики-пуки!";
+    }
+}
